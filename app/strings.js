1abd1dc197af5e26cf42d8d420df539a50d820ff
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-26 21:27:41 -0600

    Add reduceString solution

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    var newStr = '';
+    var count = 0;
 
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] !== newStr[newStr.length - 1] && str[i] === str[i + 1]) {
+        while (count < amount) {
+          newStr += str[i];
+          count++;
+        }
+        count = 0;
+      }
+      if (str[i] !== str[i - 1] && str[i] !== str[i + 1]) {
+        newStr += str[i];
+      }
+    }
+    return newStr;
   },
 
   wordWrap: function(str, cols) {
