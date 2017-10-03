1f526669bfe02a63c052dbe5ca1c580b4e180982
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-03 12:27:07 -0600

    Add array square solution

--- app/arrays.js
+++ app/arrays.js
@@ -90,7 +90,12 @@ exports.arraysAnswers = {
   },
 
   square: function(arr) {
+    var newArr = [];
 
+    for (var i = 0; i < arr.length; i++) {
+      newArr.push(arr[i] * arr[i]);
+    }
+    return newArr;
   },
 
   findAllOccurrences: function(arr, target) {
