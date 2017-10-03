64213e31a83db5242f63288b49da0de7428a7435
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-03 11:51:10 -0600

    Add array concat solution

--- app/arrays.js
+++ app/arrays.js
@@ -63,7 +63,10 @@ exports.arraysAnswers = {
   },
 
   concat: function(arr1, arr2) {
-
+    for (var i = 0; i < arr2.length; i++) {
+      arr1.push(arr2[i]);
+    }
+    return arr1;
   },
 
   insert: function(arr, item, index) {
