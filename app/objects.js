25e112dd31dd7275ed274830a4753ae6da31f130
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-04 16:47:35 -0600

    Add objects alterContext solution

--- app/objects.js
+++ app/objects.js
@@ -1,8 +1,10 @@
 exports = typeof window === 'undefined' ? global : window;
 
+/* eslint prefer-reflect: ["error", { "exceptions": ["call"] }] */
+
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
